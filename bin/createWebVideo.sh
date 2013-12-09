84aa7982193b7c087e05344905afa4189a54cca4
Florian Dietrich <jfd@daenen4.de>
2013-12-09 08:55:05 +0100

    Optimized web video compression options / fixed bug when omitting size option

--- bin/createWebVideo.sh
+++ bin/createWebVideo.sh
@@ -4,7 +4,8 @@ SOURCE=$1
 START=$2
 DURATION=$3
 SIZE=$4
-FPS=$5
+FPS=${5-12}
+
 
 TIMESTAMP=$(date +%s)
 OUTPUTDIR=output/video/$SOURCE
@@ -21,9 +22,9 @@ fi
 
 #Encode mp4
 if [ -n "$SIZE" ]; then
-	ffmpeg -i $SOURCE -ss $START -t $DURATION -s $SIZE -r $FPS -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -threads 0 -an $OUTPUTDIR/$SOURCE-$TIMESTAMP.mp4
+	ffmpeg -i $SOURCE -ss $START -t $DURATION -s $SIZE -r $FPS -codec:v libx264 -preset slow -crf 27 -threads 0 -an $OUTPUTDIR/$SOURCE-$TIMESTAMP.mp4
 #	ffmpeg -i $SOURCE -ss $START -t $DURATION -an -s $SIZE -aq 5 -ac 2 -qmax 25 -threads 2 -r $FPS $OUTPUTDIR/$SOURCE-$TIMESTAMP.mp4
 else
-	ffmpeg -i $SOURCE -ss $START -t $DURATION -r $FPS -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -threads 0 -an $OUTPUTDIR/$SOURCE-$TIMESTAMP.mp4
+	ffmpeg -i $SOURCE -ss $START -t $DURATION -r $FPS -codec:v libx264 -preset slow -crf 27 -threads 0 -an $OUTPUTDIR/$SOURCE-$TIMESTAMP.mp4
 #	ffmpeg -i $SOURCE -ss $START -t $DURATION -an -aq 5 -ac 2 -qmax 25 -threads 2 -r $FPS $OUTPUTDIR/$SOURCE-$TIMESTAMP.mp4
 fi
